a8068389dff0e0ffb6237db2b4a861ccb8d356e0
yassineboucham <yassineboucham11@gamil.com>
2023-12-14 22:08:21 +0000

    all

--- bytecodes/06.m
+++ bytecodes/06.m
@@ -2,5 +2,6 @@ push 1
 pint
 push 2
 pint
+hhh
 push 3
 pint
--- main.c
+++ main.c
@@ -10,39 +10,39 @@ int main(int argc, char **argv)
 	FILE *file;
 	size_t len = 0;
 	char *str, *func, *s;
-
-	unsigned int nb, i = 0;
-
+	unsigned int nb, i = 0, opcode_not_found = 0, j = 0;
 	stack_t *stack = NULL;
-
 	instruction_t list[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{NULL, NULL}
-	};
+		{"push", push}, {"pall", pall}, {"pint", pint}, {NULL, NULL} };
 
 	if (argc != 2)
-		return (0);
+		fputs("USAGE: monty file", stdout), exit(EXIT_FAILURE);
 	file = fopen(argv[1], "r");
 	if (!file)
-		return (0);
+	{
+		fprintf(stderr, "%s %s\n", "Error: Can't open file ", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 	while (getline(&str, &len, file) != -1)
 	{
-		func = strtok(str, " \n\t");
-	if (func != NULL)
-	{
-	s = strtok(NULL, " \n\t");
-	if (s)
-	nb = atoi(s);
-	i = 0;
-	while (list[i].opcode)
-	{
-		if (strcmp(func, list[i].opcode) == 0)
-			list[i].f(&stack, nb);
-		i++;
-	}
-	}
+		func = strtok(str, " \n\t"), j++;
+		if (func != NULL)
+		{
+			s = strtok(NULL, " \n\t"), i = 0;
+			if (s)
+				nb = atoi(s);
+			while (list[i].opcode)
+			{
+				if (strcmp(func, list[i].opcode) == 0)
+					opcode_not_found = 1, list[i].f(&stack, nb);
+				i++;
+			}
+			if (opcode_not_found == 0)
+			{
+				fprintf(stderr, "L%d: %s %s\n", j, "unknown instruction", func);
+				exit(EXIT_FAILURE);
+			} opcode_not_found = 0;
+		}
 	}
 	fclose(file);
 	return (0);
